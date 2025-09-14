1598cffc6d8a580ebe5e2ccfc2a37539cbd98b42
Yair Lifshitz <yair@lifshitz.io>
2025-09-14 23:22:43 +0300

    Integrate homegrown diarization (#9)

--- ivrit/__init__.py
+++ ivrit/__init__.py
@@ -3,7 +3,10 @@ ivrit - Python package providing wrappers around ivrit.ai's capabilities
 """
 
 from importlib.metadata import version, PackageNotFoundError
-__version__ = version("ivrit")
+try:
+    __version__ = version("ivrit")
+except:
+    __version__ = 'dev'
 
 from .audio import load_model, TranscriptionModel, TranscriptionSession, FasterWhisperModel, StableWhisperModel, RunPodModel
 from .types import Segment
